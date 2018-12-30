5624359bdfb50505119d65a45a91818e47f0ae94
TuLaThao <phamthithuthao_t60@hus.edu.vn>
2018-12-30 12:35:00 +0700

    optimize code

--- src/lighting_compensation.py
+++ src/lighting_compensation.py
@@ -1,5 +1,5 @@
-from skimage import morphology
-from scipy import ndimage, signal
+from skimage import morphology, filters
+from scipy import signal
 import numpy as np
 import cv2
 
@@ -9,45 +9,24 @@ def fn_lighting_compensation(img):
     [height, width, _] = img.shape
     num_mid_gray = np.sum(np.sum((gray_image < 240) & (gray_image > 15)))
     if num_mid_gray < 0.1 * height * width:
-        [_, bw_img] = cv2.threshold(gray_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        thresh = filters.threshold_otsu(gray_image)
+        bw_img = gray_image <= thresh
     else:
         win_size = round(min(height / 60, width / 60))
 
         window_means = signal.fftconvolve(gray_image, np.ones((win_size, win_size)) * (1 / win_size ** 2), mode='same')
         demeaned = window_means.astype(int) - gray_image.astype(int) - 10
-        demeaned[demeaned < 0] = 0
-        demeaned = demeaned.astype(np.uint8)
+        bw_img = demeaned > 0
 
-        [_, bw_img] = cv2.threshold(demeaned, 0, 255, cv2.THRESH_BINARY)
-        bw_img = bw_img.astype(bool)
         noise_size = round(0.0001 * height * width)
 
         bw_img = morphology.remove_small_objects(bw_img, min_size=noise_size)
-        bw_img = ndimage.binary_closing(bw_img, structure=np.ones((4, 4)))
+        bw_img = morphology.closing(bw_img, np.ones((5, 5)))
 
         small_hole_thresh = round(0.0001 * height * width)
-        filled = ndimage.binary_fill_holes(bw_img)
-        holes = filled & ~bw_img
-        lg_holes = morphology.remove_small_objects(holes, min_size=small_hole_thresh)
-        sm_holes = holes & ~lg_holes
-        bw_img = bw_img | sm_holes
-        bw_img = bw_img.astype(np.uint8) * 255
+        bw_img = morphology.remove_small_holes(bw_img, area_threshold=small_hole_thresh)
 
     bw_img = ~bw_img
 
     return bw_img
 
-
-def fspecial(shape=(3, 3), sigma=0.5):
-    """
-    2D gaussian mask - should give the same result as MATLAB's
-    fspecial('gaussian',[shape],[sigma])
-    """
-    m, n = [(ss - 1.) / 2. for ss in shape]
-    y, x = np.ogrid[-m:m + 1, -n:n + 1]
-    h = np.exp(-(x * x + y * y) / (2. * sigma * sigma))
-    h[h < np.finfo(h.dtype).eps * h.max()] = 0
-    sumh = h.sum()
-    if sumh != 0:
-        h /= sumh
-    return h
--- test.py
+++ test.py
@@ -5,9 +5,9 @@ from src.squeeze_matlab_matrix import *
 from src.lighting_compensation import *
 import cv2
 
-img = cv2.imread('data/Equations/Images/demo_equation2_hr.jpg')
+img = cv2.imread('data/Equations/Images/eq2_hr.jpg')
 bw_img = fn_lighting_compensation(img)
 
-cv2.imshow('test', bw_img)
+cv2.imshow('test', bw_img.astype(np.uint8) * 255)
 cv2.waitKey()
 
